e533282d95662c85800fcef4204ec844d9405570
Luca Fulgenzi <fulgenzi.luca@gmail.com>
2021-03-09 17:57:07 +0100

    first commit

---
+++ docker-clear.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh
+BLUE='\033[34m'
+
+docker stop $(docker ps -a -q) 
+docker rm $(docker ps -a -q)
+docker system prune --volumes
+
+
+echo -e "\n${BLUE}"
+cat << _EOF_
+              __   __               
+             __ \ / __             
+            /  \ | /  \           
+                \|/              
+           _,.---v---._         
+   /\__/\  /            \          
+   \_  _/ /              \         
+     \ \_|           @ __|         
+      \                \_          
+       \     ,__/       /         
+    ~~~~~~~~~~~~~~~~~~~/~~~~       
+_EOF_
